c9da652f6ba3b09ac58b07cee2f0556573654122
VazManYan <vazgenar.1993@gmail.com>
2023-03-01 14:45:45 +0200

    Home Page Finish

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,33 +1,37 @@
 import React, { useState } from "react";
 import "./Home.css";
+import Slide from "./Slide";
 function Home(){
     const[index,setIndex] = useState(0);
     return(
         <div className="Home">
             
           <div className="tabList">
-                <div className="tabHeadWine" onClick={()=>{setIndex(0)}}>
+                <div className="tabHeadWine" onClick={()=>{setIndex(1)}}>
                     <p>WINE</p>
                 </div>
-                <div className="tabHeadCheese" onClick={()=>{setIndex(1)}}>
+                <div className="tabHeadCheese" onClick={()=>{setIndex(2)}}>
                     <p>CHEESE</p>
                 </div>
-                <div className="tabHeadFish" onClick={()=>{setIndex(2)}}>
+                <div className="tabHeadFish" onClick={()=>{setIndex(3)}}>
                     <p>FISH</p>
                 </div>
-                <div className="tabHeadSalad" onClick={()=>{setIndex(3)}}>
+                <div className="tabHeadSalad" onClick={()=>{setIndex(4)}}>
                     <p>SALAD</p>
                 </div>
           </div>
-          <div className="tabHeadWineInfo" hidden = {index !== 0}>
+          <div className="SlideDiv" hidden = {index !==0}>
+            <Slide/>
+          </div>
+          <div className="tabHeadWineInfo" hidden = {index !== 1}>
                 <h1>WINE INFO</h1>
           </div>
-          <div className="tabHeadCheeseInfo" hidden = {index !== 1}>
+          <div className="tabHeadCheeseInfo" hidden = {index !== 2}>
                 <h1>CHEESE INFO</h1>
           </div>
-          <div className="tabHeadSaladInfo" hidden = {index !== 2}>
+          <div className="tabHeadSaladInfo" hidden = {index !== 3}>
                 <h1>FISH INFO</h1>
-          </div><div className="tabHeadFishInfo" hidden = {index !== 3}>
+          </div><div className="tabHeadFishInfo" hidden = {index !== 4}>
                 <h1>SALAD INFO</h1>
           </div>
           
---
+++ src/pages/Slide.js
@@ -0,0 +1,33 @@
+import ImageSlider from "./ImageSlider";
+const Slide = () => {
+  const slides = [
+    { url: "http://localhost:3000/SlideImages/Slide1.jpg"},
+    { url: "http://localhost:3000/SlideImages/Slide2.jpg"},
+    { url: "http://localhost:3000/SlideImages/Slide3.jpg"},
+    { url: "http://localhost:3000/SlideImages/Slide4.jpg"},
+    { url: "http://localhost:3000/SlideImages/Slide5.jpg"},
+    { url: "http://localhost:3000/SlideImages/Slide6.jpg"},
+    { url: "http://localhost:3000/SlideImages/Slide7.jpg"},
+    { url: "http://localhost:3000/SlideImages/Slide8.jpg"},
+    
+    
+    
+  ];
+  const containerStyles = {
+    width: "1360px",
+    height: "420px",
+   
+  };
+  return (
+    <div>
+      <div style={containerStyles}>
+        <ImageSlider slides={slides} parentWidth={1360} />
+      </div>
+    </div>
+  );
+};
+
+export default Slide;
+
+
+ 
